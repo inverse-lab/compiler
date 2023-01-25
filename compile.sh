ff5ab6050092b66722f3daf3026522b47f47f32c
zaidannew <118640080+zaidannew@users.noreply.github.com>
2023-01-26 01:30:59 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
 cd ${KERNEL_ROOTDIR}
-rm -rf out
-export ARCH=arm64
-export CC=clang
-mkdir -p O=out
-make O=out clean
-make $KERNEL_DEFCONFIG O=out
-make menuconfig O=out
-     
-build_message "Compile Started 👹  !!1!️"
-
-make -j$(nproc --all) ARCH=arm64 O=out \
-        CC=${CLANG_ROOTDIR}/bin/clang \
-        CLANG_TRIPLE=aarch64-linux-gnu \
-        CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
-        CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
+make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
+make -j$(nproc) ARCH=arm64 O=out \
+	CC=${CLANG_ROOTDIR}/bin/clang \
+	CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
+	CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
 if ! [[ -f "$IMG_PATH/Image.gz-dtb" || -f "$IMG_PATH/Image" ]]; then
      tg_send_message --chat_id "$CHAT_ID" --text "   ====== Compile Aborted 😓 ======
    👿   Please Check the eror   👿"
@@ -26,8 +16,8 @@ else
 fi
 cd $ANYKERNELDIR
 nano anykernel.sh
-cp "$IMGPATH/Image" "$ANYKERNELDIR" || cp  "$IMGPATH/Image.gz-dtb" "$ANYKERNELDIR"
-zip -r9 "GreenForce-main-${DATE}.zip" ./*
+cp "$IMGPATH/Image" "$ANYKERNELDIR" || cp  "$IMGPATH/dtb.img" "$ANYKERNELDIR" || cp  "$IMGPATH/dtbo.img" "$ANYKERNELDIR"
+zip -r9 "GreenForce-main-$(date +%Y%m%d-%H%M).zip" ./*
     build_message "Compile success ❤️"
     tg_send_message --chat_id "$CHAT_ID" --text "LOL WTF' Compile Success Mate ❤️
 Congratsss I'm Happy for you!!"
